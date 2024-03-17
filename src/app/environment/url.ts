ec478090308d7ab62eea5dc793e659e3e42f0ef2
soumyaranjan mahapatra <soumyaranjanmahapatra@soumyaranjans-MacBook-Air.local>
2024-03-17 17:42:14 +0530

    changes for angular

--- src/app/environment/url.ts
+++ src/app/environment/url.ts
@@ -5,5 +5,5 @@ const transactionUrl = "http://localhost:9090/transaction";
 export const environment = {
   production: false,
   baseUrl: baseUrl,
-  saveCreditUrl: `${transactionUrl}/saveCreditDetails`,
+  saveCreditUrl: `${baseUrl}/transaction/saveCreditDetails`,
 };
